dc16151efb2796661ffeb7e0a73ce8d64a93ff01
Peter WONG <peter.wong@gogotech.hk>
2023-06-12 16:13:55 +0800

    bluetooth result is immutable

--- flutter_device_searcher/lib/search_result/bluetooth_result.dart
+++ flutter_device_searcher/lib/search_result/bluetooth_result.dart
@@ -1,8 +1,21 @@
+import 'package:flutter/foundation.dart';
+
+@immutable
 class BluetoothResult {
-  BluetoothResult(this.address);
+  const BluetoothResult(this.address);
 
   final String address;
 
   @override
   String toString() => 'BluetoothResult{address: $address}';
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is BluetoothResult &&
+          runtimeType == other.runtimeType &&
+          address == other.address;
+
+  @override
+  int get hashCode => address.hashCode;
 }
