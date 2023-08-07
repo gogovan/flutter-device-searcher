4b120650434859166ea5f303e8a283302be2a3e0
Peter WONG <peter.wong@gogotech.hk>
2023-08-07 16:47:53 +0800

    remove DCM

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Applications/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/peterwong/Documents/flutter_device_searcher/flutter_device_searcher"
+export "FLUTTER_APPLICATION_PATH=/Users/peterwong/Documents/flutter_device_searcher"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
