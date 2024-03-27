9ee388a305c356b88b8f546206c8609be2f7a871
Peter WONG <peter.wong@gogotech.hk>
2024-03-27 16:51:46 +0800

    use quickUsb instead

--- lib/device_searcher/usb_searcher.dart
+++ lib/device_searcher/usb_searcher.dart
@@ -1,29 +1,41 @@
+import 'dart:async';
+
 import 'package:flutter_device_searcher/device_searcher/device_searcher_interface.dart';
 import 'package:flutter_device_searcher/search_result/usb_result.dart';
-import 'package:usb_serial/usb_serial.dart';
+import 'package:quick_usb/quick_usb.dart';
 
 class UsbSearcher extends DeviceSearcherInterface<UsbResult> {
   @override
   Stream<List<UsbResult>> search({
     Duration timeout = Duration.zero,
     void Function()? onTimeout,
-  }) =>
-      UsbSerial.listDevices()
-          .asStream()
+  }) async* {
+    await QuickUsb.init();
+
+    var start = DateTime.now();
+    while (true) {
+      if (timeout > Duration.zero && DateTime.now().difference(start) > timeout) {
+        onTimeout?.call();
+        break;
+      }
+      final devices = await QuickUsb.getDevicesWithDescription(requestPermission: false);
+      final result = devices
           .map(
-            (event) => event
-                .map(
-                  (e) => UsbResult(
-                    id: e.deviceId?.toString() ?? '',
-                    name: e.deviceName,
-                  ),
-                )
-                .toList(),
+            (e) => UsbResult(
+              id: e.device.identifier,
+              manufacturer: e.manufacturer,
+              product: e.product,
+            ),
           )
-          .timeout(
-        timeout,
-        onTimeout: (_) {
-          onTimeout?.call();
-        },
-      );
+          .toList();
+      if (result.isNotEmpty) {
+        start = DateTime.now();
+      }
+      yield result;
+
+      await Future.delayed(const Duration(seconds: 1));
+    }
+
+    await QuickUsb.exit();
+  }
 }
--- lib/search_result/usb_result.dart
+++ lib/search_result/usb_result.dart
@@ -3,9 +3,11 @@ import 'package:flutter_device_searcher/search_result/device_search_result.dart'
 class UsbResult implements DeviceSearchResult {
   const UsbResult({
     required this.id,
-    this.name,
+    this.manufacturer,
+    this.product,
   });
 
   final String id;
-  final String? name;
+  final String? manufacturer;
+  final String? product;
 }
