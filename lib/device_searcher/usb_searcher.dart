fba3e1c50865a411a9539727a88b14ab61da8351
Peter WONG <peter.wong@gogotech.hk>
2024-07-16 16:54:50 +0800

    fix format

--- lib/device_searcher/usb_searcher.dart
+++ lib/device_searcher/usb_searcher.dart
@@ -21,19 +21,20 @@ class UsbSearcher extends DeviceSearcherInterface<UsbResult> {
       final devicesJson =
           await FlutterDeviceSearcherPlatform.instance.searchUsb();
       final devicesObj = jsonDecode(devicesJson) as Map<String, dynamic>;
-      final result = devicesObj
-          .map(
-            (k, e) => MapEntry(k, UsbResult(
-              index: int.parse(k),
-              deviceName: e['deviceName'] as String,
-              vendorId: e['vendorId'] as String?,
-              productId: e['productId'] as String?,
-              serialNumber: e['serialNumber'] as String?,
-              deviceClass: e['deviceClass'] as String?,
-              deviceSubclass: e['deviceSubclass'] as String?,
-              deviceProtocol: e['deviceProtocol'] as String?,
-            ),
+      final result = devicesObj.map(
+        (k, e) => MapEntry(
+          k,
+          UsbResult(
+            index: int.parse(k),
+            deviceName: e['deviceName'] as String,
+            vendorId: e['vendorId'] as String?,
+            productId: e['productId'] as String?,
+            serialNumber: e['serialNumber'] as String?,
+            deviceClass: e['deviceClass'] as String?,
+            deviceSubclass: e['deviceSubclass'] as String?,
+            deviceProtocol: e['deviceProtocol'] as String?,
           ),
+        ),
       );
 
       if (result.isNotEmpty) {
